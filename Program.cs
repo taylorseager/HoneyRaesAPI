c25b05cb5fc76640a2382dd14fc1a00e4dfb1082
Taylor Seager <taylorseager718@gmail.com>
2024-07-22 21:44:10 -0500

    updated serviceTickets list and added app.MapGet for servicetickets

--- Program.cs
+++ Program.cs
@@ -14,16 +14,16 @@ List<Customer> customers = new List<Customer>()
 List<Employee> employees = new List<Employee>
 {
     new Employee(id: 1, firstName: "Joey", specialty: "Acting"),
-    new Employee(id: 2, firstName: "Ross", specialty: "History"),
+    new Employee(id: 2, firstName: "Ross", specialty: "History/Dinosaurs"),
 };
 
 List<ServiceTicket> serviceTickets = new List<ServiceTicket>
 {
     new ServiceTicket(id: 123, customerId: 1, employeeId: 2, description: "Customer is struggling with WW2 topics.", emergency: true, dateCompleted: null),
     new ServiceTicket(id: 124, customerId: 2, employeeId: 1, description: "Needs acting classes", emergency: false, dateCompleted: "07/14/2024"),
-    new ServiceTicket(id: 125, customerId: 3, employeeId: 0, description: "Customer is struggling with WW2 topics.", emergency: false, dateCompleted: "07/20/2024"),
-    new ServiceTicket(id: 126, customerId: 1, employeeId: 1, description: "Customer is struggling with WW2 topics.", emergency: false, dateCompleted: "07/21/2024"),
-    new ServiceTicket(id: 127, customerId: 2, employeeId: 0, description: "Customer is struggling with WW2 topics.", emergency: true, dateCompleted: null),
+    new ServiceTicket(id: 125, customerId: 3, employeeId: 0, description: "Wants to learn more about dinos.", emergency: false, dateCompleted: "07/20/2024"),
+    new ServiceTicket(id: 126, customerId: 1, employeeId: 1, description: "Has a casting call and needs some pointers.", emergency: false, dateCompleted: "07/21/2024"),
+    new ServiceTicket(id: 127, customerId: 2, employeeId: 0, description: "Got a call back and needs to run lines ASAP.", emergency: true, dateCompleted: null),
 
 };
 
@@ -47,32 +47,13 @@ var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
-// BELOW CREATES AN ENDPOINT - ROUTE AND HANDLER (FUNCTION THAT DETERMINES THE LOGIC FOR
-// WHAT TO DO WHEN A REQUEST IS MADE TO THAT ROUTE
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
-app.MapGet("/hello", () =>
+app.MapGet("/servicetickets", () =>
 {
-    return "hello";
+    return serviceTickets;
 });
+
+
 
 // always make sure this is at the end of the file:
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
