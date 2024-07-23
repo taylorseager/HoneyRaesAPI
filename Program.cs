4f7cb9264d3743de23fe9b2f6fd6e517acc809f3
Taylor Seager <taylorseager718@gmail.com>
2024-07-22 21:39:40 -0500

    updated lists

--- Program.cs
+++ Program.cs
@@ -1,4 +1,31 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Net;
+using HoneyRaesAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+List<Customer> customers = new List<Customer>()
+{
+    new Customer(id: 1, firstName: "Rachel", address: "123 Main St, NYC"),
+    new Customer(id: 2, firstName: "Monica", address: "146 First St, NYC"),
+    new Customer(id: 3, firstName: "Chandler", address: "231 Central Perk Dr, NYC"),
+
+};
+
+List<Employee> employees = new List<Employee>
+{
+    new Employee(id: 1, firstName: "Joey", specialty: "Acting"),
+    new Employee(id: 2, firstName: "Ross", specialty: "History"),
+};
+
+List<ServiceTicket> serviceTickets = new List<ServiceTicket>
+{
+    new ServiceTicket(id: 123, customerId: 1, employeeId: 2, description: "Customer is struggling with WW2 topics.", emergency: true, dateCompleted: null),
+    new ServiceTicket(id: 124, customerId: 2, employeeId: 1, description: "Needs acting classes", emergency: false, dateCompleted: "07/14/2024"),
+    new ServiceTicket(id: 125, customerId: 3, employeeId: 0, description: "Customer is struggling with WW2 topics.", emergency: false, dateCompleted: "07/20/2024"),
+    new ServiceTicket(id: 126, customerId: 1, employeeId: 1, description: "Customer is struggling with WW2 topics.", emergency: false, dateCompleted: "07/21/2024"),
+    new ServiceTicket(id: 127, customerId: 2, employeeId: 0, description: "Customer is struggling with WW2 topics.", emergency: true, dateCompleted: null),
+
+};
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
